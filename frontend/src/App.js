65589edaea1020ad76f5dcea715447128c172f25
Mina Nagy <minanagy1993@gmail.com>
2020-05-07 23:03:21 +0200

    Add Routing and TabsBootStrap for Admin Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Signup from './components/signup'
 import Home from './routes/home/home';
 import Admin from './routes/admin/admin';
-
+import {BrowserRouter,Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      {/*<Home />*/}
-      <Admin />
+      <BrowserRouter>
+          <switch>
+            <Route path="/admin" component={Admin} />
+            <Route exact path="/" component={Home} />
+          </switch>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/routes/admin/admin.js
+++ frontend/src/routes/admin/admin.js
@@ -1,19 +1,72 @@
 import React from 'react'
 import './admin.css'
-import logo from '../../assets/header-logo.png'
- const Admin = () => {
+import {NavLink} from 'react-router-dom'
+import { Tabs,Tab,Table} from 'react-bootstrap';
+const Admin = () => {
+    const deps=[{"DepartmentID":"1","DepartmentName":"Mina"},
+    {"DepartmentID":"2","DepartmentName":"Marco"}];
     return (
-        <ul class="nav nav-tabs">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Categories</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Books</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Authors</a>
-            </li>
-        </ul>
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+          <Tab eventKey="home" title="Home">
+          <p>Home</p>
+          </Tab>
+          <Tab eventKey="profile" title="Profile">
+            <p>Profile</p>
+          </Tab>
+          <Tab eventKey="contact" title="Contact">        
+                <Table className="mt-4" striped bordered hover size='sm'> 
+                    <thead>
+                        <tr>
+                            <th>DepartmentID</th>
+                            <th>DepartmentName</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                {
+                    deps.map(dep=>
+                        <tr key={dep.DepartmentID}>
+                        <td>{dep.DepartmentID}</td>
+                        <td>{dep.DepartmentName}</td>
+                        </tr>
+                        )
+                }
+                    </tbody>
+                </Table>
+          </Tab>
+        </Tabs>
     );
 }
 export default Admin;
+/*<ul className="nav nav-tabs">
+            <li className="nav-item">
+                <a className="nav-link active" href="#">Categories</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Books</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Authors</a>
+            </li>
+    </ul>*/
+
+/*
+<Table className="mt-4" striped bordered hover size='sm'> 
+    <thead>
+        <tr>
+            <th>Mina</th>
+            <th>Marca</th>
+        </tr>
+    </thead>
+    <tbody>
+{
+    deps.map(dep=>
+        <tr key={dep.DepartmentID}>
+         <td>{dep.DepartmentID}</td>
+         <td>{dep.DepartmentID}</td>
+         </tr>
+        )
+}
+    </tbody>
+</Table>
+
+*/
