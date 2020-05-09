5d238647b724826b1bc2c623b5607fd1ff688099
Mina Nagy <minanagy1993@gmail.com>
2020-05-09 07:01:41 +0200

    Admin Page V1.2 Add Modal for Edit and Add Buttons forEach tab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Signup from './components/signup'
 import Home from './routes/home/home';
+import Admin from './routes/admin/admin';	
+import { BrowserRouter, Route } from 'react-router-dom';
 import UserPage from './routes/UserPage/userpage';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
 import '../node_modules/jquery/dist/jquery';
 import '../node_modules/react-popper/dist/index.umd.js';
 import '../node_modules/popper.js';
-
-import { BrowserRouter, Route } from 'react-router-dom';
-
 function App() {
   return (
 
@@ -18,7 +17,7 @@ function App() {
       <div className="App">  
         <Route exact path="/" component={Home}  ></Route>
         <Route exact path="/userpage" component={UserPage}></Route>
-    
+        <Route exact path="/admin" component={Admin} />
       </div>
    
     </BrowserRouter>
