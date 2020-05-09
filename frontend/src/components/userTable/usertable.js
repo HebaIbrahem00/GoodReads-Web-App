3834aa9ed3d443abad776924e49bb95fb3770298
HeBa <heba.Ibrahem.x@gmail.com>
2020-05-09 02:38:13 +0200

    Add files via upload
    UserPage main components without pagination.

---
+++ frontend/src/components/userTable/usertable.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+
+
+
+/** started on 8/5/2020 */
+/**  we can type "rfc" and then enter and it will create a default react functional component, that's because of ES7 react Extension */
+
+export default function UserTable(props) {
+    return (
+
+        <div className="card text-center">
+            <div className="card-header">
+                <ul className="nav nav-pills card-header-pills">
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">All</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="card-body">
+                <table className="table  table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Cover</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Avg Rating</th>
+                            <th scope="col">Rating</th>
+                            <th scope="col">Shelve</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>gffg</td>
+                            <td>fgfdfg</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
+    )
+}
