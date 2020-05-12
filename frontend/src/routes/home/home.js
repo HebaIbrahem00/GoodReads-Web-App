a52bcb5b6b5b8649661262232f01b024d56f94dd
mofath <mohamed.elgaedi@gmail.com>
2020-05-12 22:11:37 +0200

    popular books and authors developed
    popular books and authors components added to home page

--- frontend/src/routes/home/home.js
+++ frontend/src/routes/home/home.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import './home.css'
 import logo from '../../assets/header-logo.png'
+import ad from '../../assets/ad.jpg'
 import headline from '../../assets/headline.png'
-import background from '../../assets/background.jpg'
 import Signup from '../../components/signup/signup'
 import Login from '../../components/login/login';
-import Footer from '../../components/footer/footer'
+import Populars from '../../components/populars/populars';
+
+
 
 const Home = () => {
     return (
         <div className='wrapper'>
+             <div className='login-signup-container'>
             <div className='container'>
+
                 <section className='header'>
                     <div className='row'>
                         {/* **************** logo ***************** */}
@@ -22,14 +26,12 @@ const Home = () => {
                         <div className='col-md-6 login-wrapper'>
                             <Login />
                         </div>
-
                     </div>
-
                 </section>
 
                 <hr className="solid" />
 
-                <section className='row body'>
+                <section className='row sec02'>
                     <div className='col-md-7 headline-wrapper'>
                         <img src={headline} alt="headline" className='headline' />
                     </div>
@@ -39,13 +41,21 @@ const Home = () => {
                     </div>
                 </section>
 
-                <section className='footer'>
-                    <div>
-                        <Footer/>
+                <section className='row sec03'>
+                    <div className='col-md-7 '>
+                        <Populars />
+                        
+                    </div>
+
+                    <div className='col-md-5 '>
+                    <img src={ad} alt="logo" className='ad' />
                     </div>
                 </section>
+
+            </div>
             </div>
         </div>
     )
 }
+
 export default Home;
