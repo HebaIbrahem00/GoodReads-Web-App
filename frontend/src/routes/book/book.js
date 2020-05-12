26939f93186de12f7d03db0435c9101db34cdbc2
MennaAbdallah <menna.eid.95@gmail.com>
2020-05-12 04:37:29 +0200

    add book and author to routes

---
+++ frontend/src/routes/book/book.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from '../../components/navbar/navbar';
+
+export default function Book(){
+    return(
+        <div className="container">
+            <Navbar/>
+            <div className="card mb-3">
+                <div className="row no-gutters">
+                    <div className="col-4">
+                    <img src="..." className="card-img" alt="..."/>
+                    </div>
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
