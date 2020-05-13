9194b9b62131d33212c5cfd76f00825aaf9adbab
mofath <mohamed.elgaedi@gmail.com>
2020-05-13 05:17:41 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,16 @@
 const express = require('express')
 const cockieParser = require('cookie-parser')
-const mongoose = require('mongoose');
-const User = require('./models/user.model')
+var cors = require('cors')
 const app = express();
 const userRouter = require('./routes/user.route')
 
-const DB_URL = 'mongodb://localhost:27017/goodreads'
 const PORT = process.env.PORT || 5000;
-mongoose.connect(DB_URL, { useNewUrlParser: true }, () => { console.log('succcessfully connected to mongodb') })
 
 app.use(cockieParser());
 app.use(express.json())
+app.use(cors())
 
 app.use('/user', userRouter)
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-
-// const userInput = {
-//     username: 'mhmdft',
-//     email: "mo@SpeechGrammarList.com",
-//     password: '123456789'
-// }
-
-// const user = new User(userInput);
-
-// user.save((err, document) => {
-//     if (err) console.log(err);
-//     console.log(document);
-
-// })
 
 
 app.listen(PORT, () =>
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,7 +3,9 @@ const passport = require('passport');
 const passportConfig = require('../passport');
 const JWT = require('jsonwebtoken')
 const User = require('../models/user.model');
-const Todo = require('../models/todo.model');
+const mongoose = require('mongoose');
+
+const DB_URL = 'mongodb://localhost:27017/goodreads'
 
 const signToken = (userID) => {
     return JWT.sign({
@@ -16,18 +18,19 @@ const signToken = (userID) => {
 }
 
 router.post('/signup', (req, res) => {
-    const { username, password, role } = req.body;
-    console.log({ username, password, role });
+    console.log('signup');
 
-    User.findOne({ username }, (err, user) => {
-        console.log('find');
+    const { username, email, password, role } = req.body;
+    mongoose.connect(DB_URL, { useNewUrlParser: true }, () => { console.log('succcessfully connected to mongodb') })
 
+
+    User.findOne({ email }, (err, user) => {
         if (err)
             res.status(500).json({ message: { msgBody: "Error has occured", msgError: true } })
         if (user)
-            res.status(400).json({ message: { msgBody: 'Username is already taken', msgErroe: true } })
+            res.status(400).json({ message: { msgBody: 'Email is already taken', msgErroe: true } })
         else {
-            const newUser = new User({ username, password, role })
+            const newUser = new User({ username, email,password, role })
             newUser.save(err => {
                 if (err)
                     res.status(500).json({ message: { msgBody: 'Error has occured', msgError: true } })
@@ -38,9 +41,7 @@ router.post('/signup', (req, res) => {
     })
 })
 
-
 router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
-    console.log('login');
     if (req.isAuthenticated()) {
         const { _id, username, role } = req.user;
         const token = signToken(_id);
@@ -55,45 +56,10 @@ router.get('/logout', passport.authenticate('jwt', { session: false }), (req, re
     res.json({ user: { username: "", role: "" }, success: true });
 });
 
-router.post('/todo', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const todo = new Todo(req.body);
-    todo.save(err => {
-        if (err)
-            res.status(500).json({ message: { msgBody: "Error has occured", msgError: true } });
-        else {
-            req.user.todos.push(todo);
-            req.user.save(err => {
-                if (err)
-                    res.status(500).json({ message: { msgBody: "Error has occured", msgError: true } });
-                else
-                    res.status(200).json({ message: { msgBody: "Successfully created todo", msgError: false } });
-            });
-        }
-    })
+
+router.get('/authenticated', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { username, role } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { username, role } });
 });
-
-router.get('/todos', passport.authenticate('jwt', { session: false }), (req, res) => {
-    User.findById({ _id: req.user._id }).populate('todos').exec((err, document) => {
-        if (err)
-            res.status(500).json({ message: { msgBody: "Error has occured" }, msgErroe: true })
-        else
-            res.status(200).json({ todos: document.todos, authenticated: true })
-    })
-})
-
-router.get('/admin', passport.authenticate('jwt', {session:false}), (req, res) => {
-    if(req.user.role == 'admin'){
-        res.status(200).json({message:{msgBody:"You are an admin"}, msgError: false})
-    }
-    else{
-        res.status(403).json({message:{msgBody: "You are not an admin"}, msgError: true })
-    }
-})
-
-router.get('/authenticated',passport.authenticate('jwt',{session : false}),(req,res)=>{
-    const {username,role} = req.user;
-    res.status(200).json({isAuthenticated : true, user : {username,role}});
-});
-
 
 module.exports = router;
