d59fa6d1a6b4394476d399fb64e832e3f56dfaa3
MennaAbdallah <menna.eid.95@gmail.com>
2020-05-13 22:14:21 +0200

    add dropdown component

---
+++ frontend/src/components/dropdown/dropdown.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Dropdown(){
+    return(
+        <select className="custom-select" id="inputGroupSelect01">
+            <option value="1">Want To Read</option>
+            <option value="2">Currently Read</option>
+            <option value="3">Finish Reading</option>
+        </select>
+)
+}
