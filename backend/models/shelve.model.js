784cfb3e44540c63ee40654e7501abddb3a6279d
Heba Ibrahem <heba@latefangers.zte.com.cn>
2020-05-25 16:39:46 +0200

    added index to make shelves unique per user

--- backend/models/shelve.model.js
+++ backend/models/shelve.model.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema ;
 
-
+const bookModel = require('../models/book.model');
 
 
 const shelveSchema = new mongoose.Schema({
   user:{type: Schema.Types.ObjectId ,ref:"User"},
   book :  [{type: Schema.Types.ObjectId ,ref:"Book"}],
-  name: { type: String ,required:true , unique:true},
+  name: { type: String ,required:true },
  
 });
 
+shelveSchema.index({ name: 1, user: 1 }, { unique: true });
 
+shelveSchema.methods.addShelve = function(){
+
+}
+
+shelveSchema.methods.getShelveBooks = function(){
+
+}
 
 const shelveModel = mongoose.model('Shelve',shelveSchema);
 
