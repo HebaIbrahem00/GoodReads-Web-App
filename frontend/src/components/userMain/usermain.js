fa6f9d819386f7f799d2346f1162945e2010e06a
Heba Ibrahem <heba@latefangers.zte.com.cn>
2020-05-27 03:27:37 +0200

    changing props

--- frontend/src/components/userMain/usermain.js
+++ frontend/src/components/userMain/usermain.js
@@ -1,8 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from "react";
 
 import UserTable from "../userTable/usertable.js";
 import axios from "axios";
-import { AuthContext, AuthProvider } from "../../store/AuthContext";
+
 
 export default function UserMain(props) {
   const [shelve, setShelve] = useState("All");
@@ -24,25 +26,19 @@ export default function UserMain(props) {
       data: {
         id: id,
         shelve: shelve,
-      }
-    }).then((response) => {
-        console.log("response",response.data);
-        console.log("axios workingg");
-        setBooks(response.data)
-      }).catch((error) => {
+      },
+    })
+      .then((response) => {
+        console.log("response", response.data);
+     
+        setBooks(response.data);
+      })
+      .catch((error) => {
         console.log(error);
       });
-  },[]);
+  },[2]);
 
-  /*
- async function getBooks(url,data){
-  const response = await fetch(url,{
-    method:'get',
-    body:data
-  } );
-}*/
 
- 
 
   return (
     <div>
@@ -61,7 +57,7 @@ export default function UserMain(props) {
             >
               All
             </a>
-            <span className="badge badge-primary badge-pill">0</span>
+            <span className="badge badge-primary badge-pill"> {}</span>
           </li>
           <li
             className="list-group-item d-flex justify-content-between align-items-center"
